fcab4da282b879552800835167f805c832074d9d
JohnMata0427 <john.mata@epn.edu.ec>
2024-08-06 19:30:11 -0500

    Deployment config and add .env.example variables

--- src/database.js
+++ src/database.js
@@ -1,12 +1,16 @@
-import { set as setOptions, connect } from 'mongoose'
+import { set as setOptions, connect } from "mongoose";
 
-setOptions('strictQuery', true)
+setOptions("strictQuery", true);
 
 export default async () => {
-    try {
-        const { connection } = await connect(process.env.MONGODB_URI_LOCAL)
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
-    } catch (error) {
-        console.error(error)
-    }
-}
+	try {
+		const { connection } = await connect(
+			process.env.MONGODB_URI_LOCAL || process.env.MONGODB_URI_PRODUCTION
+		);
+		console.log(
+			`Database is connected on ${connection.host} - ${connection.port}`
+		);
+	} catch (error) {
+		console.error(error);
+	}
+};
